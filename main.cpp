45e135d731adc853115ec330ed04928c19d0eab9
keanemind <keanemind@gmail.com>
2018-09-26 23:09:41 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cerrno>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    if (argc == 0) {
+        // Print help
+    }
+    std::cout << "Running GDB." << std::endl;
+    int fd0[2]; // PDB -> GDB
+    int fd1[2]; // GDB -> PDB
+    int pid = fork();
+    if (pid == -1) {
+        std::cout << "Fork failed." << std::endl;
+    } else if (pid == 0) {
+        // Child
+        close(fd[1]);
+        // Close stdin and duplicate the input end of the pipe to stdin
+        dup2(fd[0], 0);
+        int err = execvp("gdb", argv + 1);
+        if (err == -1) {
+            std::cout << "execvp failure:" << strerror(errno) << std::endl;
+        }
+        exit(1);
+    }
+    // Parent
+    close(fd[0]);
+    // Close stdout and duplicate the output end of the pipe to stdout
+    dup2(fd[1], 1);
+}
